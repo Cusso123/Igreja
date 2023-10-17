e8be523d4d6e8686aea2ef65806dedc64dbdf11b
034287 <034287@UNI-LAB5-129>
2023-10-16 21:54:05 -0300

    Alterações

--- Igreja.Dados/Contexto.cs
+++ Igreja.Dados/Contexto.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using Igreja.Dados.EntityFramework.Configuration;
+using Igreja.Dominio.Entidades;
+using Igreja.Dominion.Entities;
+using Microsoft.EntityFrameworkCore;
 
-public class Class1
-{
-	public Class1()
-	{
-	}
-}
+public class Contexto : DbContext
+    {
+        public DbSet<Endereco> Endereco { get; set; }
+
+        public Contexto(DbContextOptionsBuilder options) : base(options.Options)
+        {
+            options.UseSqlServer(@"Data source = 201.62.57.93:1434; 
+                                    Database = DB044323; 
+                                    User ID = RA044323; 
+                                    Password = 044323");
+
+        }
+
+        protected override void OnModeCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new EnderecoConfiguration());
+    }
+
+    }
+
